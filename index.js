40cc1712a1938625ee707fc8826421f645a83e5e
Richard Schneider <makaretu@gmail.com>
2017-12-25 12:57:35 +1300

    feat: run multiple tests

--- index.js
+++ index.js
@@ -4,11 +4,17 @@
 
 const Suite = require('benchmarked')
 const path = require('path')
+const fs = require('fs')
 
-const suite = new Suite({
-  cwd: path.join(__dirname, 'pbkdf2'),
-  fixtures: 'fixtures/*.json',
-  code: 'code/*.js',
-});
+const tests = fs.readdirSync(path.join(__dirname, 'test'))
+console.log (tests)
 
-suite.run();
+tests.forEach(t => {
+  const suite = new Suite({
+    cwd: path.join(__dirname, 'test', t),
+    fixtures: 'fixtures/*.json',
+    code: 'code/*.js'
+  })
+
+  suite.run()
+})
